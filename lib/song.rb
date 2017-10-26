44827d7f72d4d6110c79498926bff7dd53e14419
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-26 22:28:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
 
+  @@count = 0
+
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
@@ -7,5 +9,10 @@ class Song
     @artist = artist
     @genre = genre
   end
+
+  def self.count
+    @@count
+  end
+end
 
 end
